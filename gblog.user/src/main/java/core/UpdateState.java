cfa43e7c8dd576cc43149958065a376d1d93e493
algo <algo_lzw@yahoo.com>
2016-06-07 15:58:24 +0800

    change name

--- gblog.user/src/main/java/core/UpdateState.java
+++
@@ -1,5 +0,0 @@
-package core;
-
-public enum UpdateState {
-	LOGIN,LOGOUT
-}
--- gblog/src/main/webapp/js/login.js
+++ gblog/src/main/webapp/js/login.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     	var userJson={username:name,password:psd};
     	//alert(name+" "+psd);
     	$.ajax({
-    		url:"http://localhost:8080/gblog.user/user/login",
+    		url:"http://localhost:8080/guser/user/login",
     		type:"post",
     		data:userJson,
     		success:function(data){
